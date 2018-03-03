c0634425403084cc9d8d76ae91730302277e106b
Eric Masseran <rico.masseran@gmail.com>
2018-03-03 17:44:09 +0100

    Add basic skeleton with Routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Route, Link} from 'react-router-dom'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      refLangBase: {}, // Last reference for the original language
+      targetLangBase: {}, // Last translation done
+      refLangUpdate: {}, // New reference for the original language
+      targetLangUpdate: {}, // New translation in progress
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <ul>
+              <li><Link to={`${this.props.match.url}/help`}>Help</Link></li>
+              <li><Link to={`${this.props.match.url}/configure`}>Configure</Link></li>
+              <li><Link to={`${this.props.match.url}/translate`}>Translate</Link></li>
+              <li><Link to={`${this.props.match.url}/release`}>Release</Link></li>
+            </ul>
+          </header>
+          <div id="panel">
+            <Route exact path={`${this.props.match.url}/`} component={()=><h2>Home</h2>}/>
+            <Route path={`${this.props.match.url}/help`} component={()=><h2>Help</h2>}/>
+            <Route path={`${this.props.match.url}/translate`} component={()=><h2>Translate</h2>}/>
+            <Route path={`${this.props.match.url}/configure`} component={()=><h2>Configure</h2>}/>
+            <Route path={`${this.props.match.url}/release`} component={()=><h2>Release</h2>}/>
+          </div>
+        </div>
     );
   }
 }
--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router component={App}>
+    <div>
+      <Route path="/translate-web-extension" component={App}/>
+      <Route path="*" component={()=><Redirect to="/translate-web-extension" />} />
+    </div>
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
